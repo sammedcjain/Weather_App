cae4e396516e454dc3da019b810bce9a46ccf293
Sammed Jain <sammedsammy2002@gmail.com>
2023-03-12 18:40:18 +0530

    Update port in app.js

--- app.js
+++ app.js
@@ -40,6 +40,11 @@ app.post('/',function(req,res){
   });
 });
 
-app.listen(3000,function(){
-  console.log("operating in port 3000");
+let port = process.env.PORT;
+if(port==null || port ==""){
+  port=3000;
+}
+
+app.listen(port, function() {
+  console.log("Server has started successfully");
 });
